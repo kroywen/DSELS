65c2ef6c7904a0ef72d13844991324ce1f992154
kroywen <sukovMax@gmail.com>
2014-04-29 12:54:09 +0300

    Initial commit

---
+++ src/com/iscoreapp/dsels/MainScreen.java
@@ -0,0 +1,14 @@
+package com.iscoreapp.dsels;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_screen);
+	}
+
+}
